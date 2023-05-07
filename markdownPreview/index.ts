45abcee37971e00a0f9b67440f288a615ee1ad85
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-08 00:39:14 +0200

    Update index.ts

--- markdownPreview/index.ts
+++ markdownPreview/index.ts
@@ -1,25 +1,2 @@
-import mermaid from 'mermaid';
-import { renderMermaidBlocksInElement } from './mermaid';
 
-function init() {
-    const configSpan = document.getElementById('markdown-mermaid');
-    const darkModeTheme = configSpan?.dataset.darkModeTheme;
-    const lightModeTheme = configSpan?.dataset.lightModeTheme;
-
-    const config = {
-        startOnLoad: false,
-        theme: document.body.classList.contains('vscode-dark') || document.body.classList.contains('vscode-high-contrast')
-            ? darkModeTheme ?? 'dark'
-            : lightModeTheme ?? 'default'
-    };
-    mermaid.initialize(config);
-
-    renderMermaidBlocksInElement(document.body, (mermaidContainer, content) => {
-        mermaidContainer.innerHTML = content;
-    });
-}
-
-
-window.addEventListener('vscode.markdown.updateContent', init);
-
-init();
+import from './stealify.ts';
