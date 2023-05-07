ede36afc42737e712e6839b3f9c740eefe714b35
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-08 00:36:16 +0200

    Update mermaid.ts

--- markdownPreview/mermaid.ts
+++ markdownPreview/mermaid.ts
@@ -1,47 +1,6 @@
-import mermaid from 'mermaid';
-
-async function renderMermaidElement(mermaidContainer: HTMLElement, writeOut: (mermaidContainer: HTMLElement, content: string) => void) {
-    const containerId = `mermaid-container-${crypto.randomUUID()}`;
-    mermaidContainer.id = containerId;
-
-    const id = `mermaid-${crypto.randomUUID()}`;
-    const source = mermaidContainer.textContent ?? '';
-    mermaidContainer.innerHTML = '';
-
-    try {
-        mermaid.mermaidAPI.reset();
-
-        // Catch any parsing errors
-        await mermaid.parse(source);
-
-        //  Render the diagram
-        const renderResult = await mermaid.render(id, source);
-        writeOut(mermaidContainer, renderResult.svg);
-        renderResult.bindFunctions?.(mermaidContainer);
-    } catch (error) {
-        if (error instanceof Error) {
-            const errorMessageNode = document.createElement('pre');
-            errorMessageNode.className = 'mermaid-error';
-            errorMessageNode.innerText = error.message;
-            writeOut(mermaidContainer, errorMessageNode.outerHTML);
-        }
-
-        throw error;
+document.customElement.define('stealify-element',class extends HTMLElement{
+    connectedCallback() {
+        console.log('el',this.innerText);
     }
-}
+})
 
-export async function renderMermaidBlocksInElement(root: HTMLElement, writeOut: (mermaidContainer: HTMLElement, content: string) => void): Promise<void> {
-    // Delete existing mermaid outputs
-    for (const el of document.querySelectorAll('.mermaid > svg')) {
-        el.remove();
-    }
-    for (const svg of document.querySelectorAll('svg')) {
-        if (svg.parentElement?.id.startsWith('dmermaid')) {
-            svg.parentElement.remove();
-        }
-    }
-
-    for (const mermaidContainer of root.getElementsByClassName('mermaid')) {
-        await renderMermaidElement(mermaidContainer as HTMLElement, writeOut);
-    }
-}
